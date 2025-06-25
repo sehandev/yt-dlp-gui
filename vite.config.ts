bbf442ff1e09514b52d3138e3b646fc3f06cb031
Sungdon Kim <sehandev@gmail.com>
2025-06-25 16:37:27 +0900

    Update dependencies, add Tailwind CSS configuration, and implement utility functions for class management

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,56 @@
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
-// https://vitejs.dev/config/
-export default defineConfig(async () => ({
-  plugins: [react()],
+export default defineConfig({
+	plugins: [react(), tailwindcss()],
+	// prevent vite from obscuring rust errors
+	clearScreen: false,
+	server: {
+		// make sure this port matches the devUrl port in tauri.conf.json file
+		port: 1420,
+		// Tauri expects a fixed port, fail if that port is not available
+		strictPort: true,
+		// if the host Tauri is expecting is set, use it
+		host: host || false,
+		hmr: host
+			? {
+					protocol: "ws",
+					host,
+					port: 1421,
+				}
+			: undefined,
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
-  clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
-  server: {
-    port: 1420,
-    strictPort: true,
-    host: host || false,
-    hmr: host
-      ? {
-          protocol: "ws",
-          host,
-          port: 1421,
-        }
-      : undefined,
-    watch: {
-      // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: "index.html",
-        splashscreen: "splashscreen.html",
-      },
-    },
-  },
-}));
+		watch: {
+			// tell vite to ignore watching `src-tauri`
+			ignored: ["**/src-tauri/**"],
+		},
+	},
+	// Env variables starting with the item of `envPrefix` will be exposed in tauri's source code through `import.meta.env`.
+	envPrefix: ["VITE_", "TAURI_ENV_*"],
+	build: {
+		// Tauri uses Chromium on Windows and WebKit on macOS and Linux
+		target:
+			process.env.TAURI_ENV_PLATFORM === "windows" ? "chrome105" : "safari13",
+		// don't minify for debug builds
+		minify: !process.env.TAURI_ENV_DEBUG ? "esbuild" : false,
+		// produce sourcemaps for debug builds
+		sourcemap: !!process.env.TAURI_ENV_DEBUG,
+		rollupOptions: {
+			input: {
+				main: "index.html",
+				splashscreen: "splashscreen.html",
+			},
+		},
+	},
+
+	// shadcn/ui
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
